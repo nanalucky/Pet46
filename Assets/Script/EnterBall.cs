250d6d29d872f3d92cee2fca83d3ea6855fa975b
nanalucky <nana_lucky@msn.com>
2015-10-24 21:08:04 +0800

    throw ball at current point

--- Assets/Script/EnterBall.cs
+++ Assets/Script/EnterBall.cs
@@ -5,9 +5,15 @@ public class EnterBall : EnterInteract {
 
 	// Use this for initialization
 	void Start () {
+		GameObject goDog = GameObject.FindGameObjectWithTag("dog");
+		if ((Camera.main.transform.position - goDog.transform.position).magnitude > 1.0f) {
+			ToBall();
+			return;
+		}
+
 		// choose the farest lookat
-		lookat = GameObject.FindGameObjectWithTag ("dog").GetComponent<DogController> ().ChooseLookat ();
-		
+		lookat = goDog.GetComponent<DogController> ().ChooseLookat ();
+
 		aiMoveCamera = new AIMoveCamera ();
 		aiMoveCamera.Start (this);
 	}
@@ -17,17 +23,21 @@ public class EnterBall : EnterInteract {
 		aiMoveCamera.Update ();
 		if (aiMoveCamera.IsFinished ()) 
 		{
-			// ball
-			GameObject goBall = Instantiate(Resources.Load("Prefabs/Ball")) as GameObject;
-			goBall.GetComponent<Rigidbody>().isKinematic = true;
-			goBall.transform.position = Camera.main.transform.position + Camera.main.transform.rotation * (new Vector3(0, 0, 0.4f + 0.08f));
-			
-			// script
-			GameObject goPlay = Instantiate(Resources.Load("Prefabs/BallPlay")) as GameObject;
-			goPlay.transform.parent = goBall.transform;
-
-			Destroy(gameObject);
-			return;
+			ToBall();
 		}
+	}
+
+	void ToBall()
+	{
+		// ball
+		GameObject goBall = Instantiate(Resources.Load("Prefabs/Ball")) as GameObject;
+		goBall.GetComponent<Rigidbody>().isKinematic = true;
+		goBall.transform.position = Camera.main.transform.position + Camera.main.transform.rotation * (new Vector3(0, 0, 0.4f + 0.08f));
+		
+		// script
+		GameObject goPlay = Instantiate(Resources.Load("Prefabs/BallPlay")) as GameObject;
+		goPlay.transform.parent = goBall.transform;
+		
+		Destroy(gameObject);
 	}
 }
